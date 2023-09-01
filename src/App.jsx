54f90234ed0259c6c9968619e31325eb16e18be8
Itzshine <harshdambhareh53@gmail.com>
2023-09-01 15:48:40 +0530

    Added note tag

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./components/projects/Projects";
 import Skills from "./components/skills/Skills";
 import Experience from "./components/experience/Experience";
 import Contact from "./components/contact/Contact";
+import { Fragment } from "react";
 import "./index.css";
 import { useState } from "react";
 function App() {
@@ -13,19 +14,22 @@ function App() {
     contract: null,
   });
   const saveState = (state) => {
-    console.log(state)
+    console.log(state);
     setState(state);
   };
   return (
-    <>
+    <Fragment>
+      {state.web3 == null && (
+        <div className="marquee">Connect to Polygon Mumbai TestNet</div>
+      )}
       <Wallet saveState={saveState}></Wallet>
-      <Hero state={state}/>
+      <Hero state={state} />
       <Handles />
       <Projects state={state} />
       <Skills />
-      <Experience state={state}/>
+      <Experience state={state} />
       <Contact state={state} />
-    </>
+    </Fragment>
   );
 }
 
